be2a46e62eae3cd337ae9a087ffb1be17e730883
Tyler <small.tyler@yahoo.com>
2020-11-22 20:40:09 -0800

    wip, rectangles draw on click, text editor is coupled with the graph widget, need to separate

--- src/view/app.py
+++ src/view/app.py
@@ -1,38 +1,102 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
-from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
-#todo: think about decoupling the main window from the app itself
+class PlainTextEdit(QPlainTextEdit):
+    def __init__(self):
+        super().__init__()
+        self._nodes = []
+    def mousePressEvent(self, e):
+        self._nodes.append(e.pos())
+        super().mousePressEvent(e)
+        self.viewport().update()
+    def paintEvent(self, e):
+        super().paintEvent(e)
+        painter = QPainter(self.viewport())
+        for node in self._nodes:
+            rect = QRect(node, QSize(100, 100))
+            brush = QBrush(Qt.blue)
+            rectF = QRectF(node.x(), node.y(), 100, 100)
+            painter.fillRect(rectF, brush)
+            painter.drawRect(rect)
 
-app = QApplication(sys.argv)
+app = QApplication([])
+text = PlainTextEdit()
+text.setPlainText("Click with the mouse below to shoot ;-)")
 
-#retrive main window instance
-mainWindow = QMainWindow()
-mainWindow.setWindowTitle("Graphmake alpha")
+# The rest of the code is as for the normal version of the text editor.
 
-#populate window data here
-graph = QWidget(mainWindow)
-graph.setMinimumWidth(800)
-graph.setMinimumHeight(600)
+class MainWindow(QMainWindow):
+    def closeEvent(self, e):
+        if not text.document().isModified():
+            return
+        answer = QMessageBox.question(
+            window, None,
+            "You have unsaved changes. Save before closing?",
+            QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel
+        )
+        if answer & QMessageBox.Save:
+            save()
+        elif answer & QMessageBox.Cancel:
+            e.ignore()
 
-#use qpainter to draw rectangle
-node = QWidget(graph)
-node.setMinimumWidth(100)
-node.setMinimumHeight(100)
-nodePalette = node.palette()
-nodePalette.setColor(node.backgroundRole(), Qt.blue)
-node.setPalette(nodePalette)
-node.show()
-print(node.pos().x())
+app.setApplicationName("Text Editor")
+window = MainWindow()
+window.setCentralWidget(text)
 
-#finish rendering background for graph
-graphPalette = graph.palette()
-graphPalette.setColor(graph.backgroundRole(), Qt.red)
-graph.setPalette(graphPalette)
-graph.show()
+file_path = None
 
-mainWindow.setCentralWidget(graph)
-mainWindow.show()
+menu = window.menuBar().addMenu("&File")
+open_action = QAction("&Open")
+def open_file():
+    global file_path
+    path = QFileDialog.getOpenFileName(window, "Open")[0]
+    if path:
+        text.setPlainText(open(path).read())
+        file_path = path
+open_action.triggered.connect(open_file)
+open_action.setShortcut(QKeySequence.Open)
+menu.addAction(open_action)
 
+save_action = QAction("&Save")
+def save():
+    if file_path is None:
+        save_as()
+    else:
+        with open(file_path, "w") as f:
+            f.write(text.toPlainText())
+        text.document().setModified(False)
+save_action.triggered.connect(save)
+save_action.setShortcut(QKeySequence.Save)
+menu.addAction(save_action)
+
+save_as_action = QAction("Save &As...")
+def save_as():
+    global file_path
+    path = QFileDialog.getSaveFileName(window, "Save As")[0]
+    if path:
+        file_path = path
+        save()
+save_as_action.triggered.connect(save_as)
+menu.addAction(save_as_action)
+
+close = QAction("&Close")
+close.triggered.connect(window.close)
+menu.addAction(close)
+
+help_menu = window.menuBar().addMenu("&Help")
+about_action = QAction("&About")
+help_menu.addAction(about_action)
+def show_about_dialog():
+    text = "<center>" \
+           "<h1>Text Editor</h1>" \
+           "&#8291;" \
+           "<img src=icon.svg>" \
+           "</center>" \
+           "<p>Version 31.4.159.265358<br/>" \
+           "Copyright &copy; Company Inc.</p>"
+    QMessageBox.about(window, "About Text Editor", text)
+about_action.triggered.connect(show_about_dialog)
+
+window.show()
 app.exec_()
