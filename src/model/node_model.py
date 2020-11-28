d5726e57f914052cbdaad5eeaef4cec4b6f33684
Tyler <small.tyler@yahoo.com>
2020-11-27 18:39:37 -0800

    add uids to nodes

--- src/model/node_model.py
+++ src/model/node_model.py
@@ -1,7 +1,10 @@
 from PyQt5.QtCore import *
+import uuid
+
 # Represents a node from the backend, a node will be shown on the front end by a rectangle
 class Node(QObject):
     def __init__(self):
         super().__init__()
         self.name = "Node"
         self.code = "Some Cmake Code"
+        self.guid = uuid.uuid4()
