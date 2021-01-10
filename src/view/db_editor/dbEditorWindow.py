9d7d8bd24ff74a67c35d47df9b08437cba1bc53b
Tyler <small.tyler@yahoo.com>
2021-01-09 21:40:57 -0800

    db editor gui changes

--- src/view/db_editor/dbEditorWindow.py
+++ src/view/db_editor/dbEditorWindow.py
@@ -4,28 +4,56 @@ from PyQt5.QtCore import *
 from view.nodeSelector import *
 from view.console import *
 
+class XMLViewWidget(QWidget):
+    def __init__(self, parent, view_str_name, enable_append = False):
+        super().__init__(parent)
+        # todo: dynamic variables
+        layout = QGridLayout(self)
+
+        if enable_append == False:
+            # Type
+            # Func
+            func_label = QLabel(self)
+            func_label.setText(view_str_name)
+            layout.addWidget(func_label)
+
+            func_input = QTextEdit(self)
+            layout.addWidget(func_input)
+
+        else:
+            # Var
+            var_label = QLabel(self)
+            var_label.setText("Variable")
+            layout.addWidget(var_label)
+
+            var_input = QTextEdit(self)
+            layout.addWidget(var_input)
+
+            add_more_vars_button = QPushButton()
+            add_more_vars_button.setText("Add")
+            layout.addWidget(add_more_vars_button)
+
+            remove_var_button = QPushButton()
+            remove_var_button.setText("Remove")
+            layout.addWidget(remove_var_button)
+
 class DBEditorWidget(QWidget):
     def __init__(self, parent = None):
         super().__init__(parent)
 
         layout = QGridLayout(self)
 
-        #Type
-        func_label = QLabel(self)
-        func_label.setText("Function")
-        layout.addWidget(func_label)
+        func_widget = XMLViewWidget(self, "Function")
+        layout.addWidget(func_widget)
 
-        func_input = QTextEdit(self)
-        layout.addWidget(func_input)
+        var_widget = XMLViewWidget(self, "Variable", True)
+        layout.addWidget(var_widget)
 
-        var_label = QLabel(self)
-        var_label.setText("Variable")
-        layout.addWidget(var_label)
+        insert_button = QPushButton()
+        insert_button.setText("Insert Into Database")
+        layout.addWidget(insert_button)
 
-        var_input = QTextEdit(self)
-        layout.addWidget(var_input)
-
-        #Fields
+        # Fields
 
 class DBEditorWindow(QMainWindow):
     def __init__(self, parent = None):
