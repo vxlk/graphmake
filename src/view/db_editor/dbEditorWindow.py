218969a081656c91d6dbb87fbaa92b8561e1a3a3
Tyler <small.tyler@yahoo.com>
2021-01-10 20:34:46 -0800

    shits not working

--- src/view/db_editor/dbEditorWindow.py
+++ src/view/db_editor/dbEditorWindow.py
@@ -3,39 +3,68 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from view.nodeSelector import *
 from view.console import *
+# https://stackoverflow.com/questions/28818323/qscrollarea-not-working-as-expected-with-qwidget-and-qvboxlayout
+class XMLVarButton(QWidget):
+    def __init__(self, parent, name_str):
+        super().__init__(parent)
+
+        self.layout = QGridLayout(self)
+
+        self.label = QLabel()
+        self.label.setText(name_str)
+        self.layout.addWidget(self.label)
+
+        self.var_input = QTextEdit(self)
+        self.layout.addWidget(self.var_input)
+
+        self.remove_var_button = QPushButton()
+        self.remove_var_button.setText("Remove")
+        self.layout.addWidget(self.remove_var_button)
+
+        self.setLayout(self.layout)
 
 class XMLViewWidget(QWidget):
     def __init__(self, parent, view_str_name, enable_append = False):
         super().__init__(parent)
         # todo: dynamic variables
-        layout = QGridLayout(self)
+        self.layout = QGridLayout(self)
+        self.varNum = 1
 
         if enable_append == False:
             # Type
             # Func
             func_label = QLabel(self)
             func_label.setText(view_str_name)
-            layout.addWidget(func_label)
+            self.layout.addWidget(func_label)
 
             func_input = QTextEdit(self)
-            layout.addWidget(func_input)
+            self.layout.addWidget(func_input)
 
         else:
             # Var
             var_label = QLabel(self)
             var_label.setText("Variable")
-            layout.addWidget(var_label)
-
-            var_input = QTextEdit(self)
-            layout.addWidget(var_input)
+            self.layout.addWidget(var_label)
 
             add_more_vars_button = QPushButton()
-            add_more_vars_button.setText("Add")
-            layout.addWidget(add_more_vars_button)
+            add_more_vars_button.setText("Add Variable")
+            add_more_vars_button.clicked.connect(lambda:self.AddVarButton())
+            self.layout.addWidget(add_more_vars_button)
+        
+        self.setLayout(self.layout)
+        
+        """
+        self.scroll_area = QScrollArea()
+        self.scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scroll_area.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_area.setWidget(self)
+        """
 
-            remove_var_button = QPushButton()
-            remove_var_button.setText("Remove")
-            layout.addWidget(remove_var_button)
+    def AddVarButton(self):
+        var_button = XMLVarButton(self, "Variable " + str(self.varNum))
+        self.varNum += 1
+        self.layout.addWidget(var_button)
 
 class DBEditorWidget(QWidget):
     def __init__(self, parent = None):
@@ -48,11 +77,14 @@ class DBEditorWidget(QWidget):
 
         var_widget = XMLViewWidget(self, "Variable", True)
         layout.addWidget(var_widget)
+        scroll_area = QScrollArea()
+        scroll_area.setWidget(var_widget)
+        scroll_area.setWidgetResizable(True)
+        scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
 
         insert_button = QPushButton()
         insert_button.setText("Insert Into Database")
         layout.addWidget(insert_button)
-
         # Fields
 
 class DBEditorWindow(QMainWindow):
