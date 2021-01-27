3921d9880ecfced777c49dd7284c0566adfbe9cb
vxlk <vxlk1@yahoo.com>
2021-01-26 21:38:52 -0500

    untested level list code

--- src/util/level_list.py
+++ src/util/level_list.py
@@ -1,4 +1,3 @@
-
 # A specific node in a @Level
 class LevelNode():
     def __init__(self):
@@ -22,18 +21,45 @@ class LevelNode():
 
 # A specific level in the @LevelList
 class Level():
-    def __init__(self):
+    def __init__(self, int_level_num):
         self.nodes = [] # a list of nodes at the level
-        self.level_num = 0
+        self.level_num = int_level_num
+        self.iterator = 0
 
     def First(self):
+        self.iterator += 1
         if len(self.nodes) > 0:
             return self.nodes[0]
         return None
 
-    def Insert(self):
-        # todo
-        self.nodes.append(None)
+    def Next(self):
+        self.iterator += 1
+        if (Last() < iterator)
+            return self.levels[self.iterator]
+        return None
+    
+    def Last(self):
+        index = len(self.nodes)
+        if index == 0:
+            None
+        return self.nodes[index-1]
+
+    def LastLevel(self):
+        if (len(self.nodes) > 0)
+            return self.nodes[len(self.nodes)-1]
+        return None
+
+    def LastLevelNum(self):
+        return len(self.levels)
+
+    # assume parent/child is set before inserting into the level
+    def Insert(self, node):
+        # assign neighbors
+        last_node = Last()
+        last_node.neighbor_right = node
+        node.neighbor_left = last_node
+        # add to the list now that the neighbors are assigned
+        self.nodes.append(node)
 
 # A level list is a data structure that is meant to mirror the structure of xml in code
 # it provides:
@@ -41,11 +67,62 @@ class Level():
 # a list of nodes at that level
 # a list of children under that node with their appropriate levels
 #
+# During construction: assumes all nodes added are in order that they should be in the tree
 # Supports:
-# GetNodesAtLevel()
-# GetChildren()
-# GetParents()
-# GetNode()
+# A
 class LevelList():
     def __init__(self):
         self.levels = []
+        self.iterator = 0
+
+    # 0 indexing idiot
+    def AddNode(str_name, int_level):
+        if int_level > len(self.levels)-1:
+            AddNewLevel(str_name)
+        else:
+            AddToCurrentLevel(str_name, int_level)
+
+    def AddNewLevel(str_name):
+        self.levels.append(Level(self.LastLevelNum() + 1))
+        current_level = self.LastLevel()
+        current_level.parent = self.LastLevel()  
+        self.LastLevel().child = current_level
+        current_level.xml_node_name = str_name
+
+    def AddToCurrentLevel(str_name, int_level):
+        current_level = self.levels[int_level]
+        node = LevelNode()
+        # figure out parent
+        last_level = LastLevel()
+        node.parent = last_level.Last()
+        last_level.child = node
+        current_level.Insert(node)
+        current_level.xml_node_name = str_name
+        
+    def LastLevel(self):
+        if (len(self.levels) > 0)
+            return self.levels[len(self.levels)-1]
+        return None
+
+    def LastLevelNum(self):
+        return len(self.levels)
+
+    def Next(self):
+        self.iterator += 1
+        if (LastLevelNum() < iterator)
+            return self.levels[self.iterator]
+        return None
+
+    def FirstLevel():
+        self.iterator = 0
+        if (LastLevel != None)
+            return self.levels[0]
+        else:
+            return None
+    
+
+# 0 - global_cmake --------------------
+#        |  child()          |            |
+# 1 - cmake_version,       next() global_set, scripting
+#     parent() ^| child()             |           |
+# 2 - var1 var2                    var1 var2   var1 var2
